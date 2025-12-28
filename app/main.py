60c25a052c49b677381742fc66a49b2f5c1ca854
브리즈 <ha.sungbae@breezelab.io>
2025-12-28 09:15:39 +0900

    드뎌 fastapi-demo의 github pr

--- app/main.py
+++ app/main.py
@@ -13,10 +13,10 @@ def root():
 def hello_api(name: str):
     return hello(name)
 
-## app/main.py에 슬쩍 추가해 보세요
-#def bad_logic(data):
-#        unused_var = 123  # Ruff가 잡아낼 것임
-#    return eval(data) # Bandit/Semgrep이 잡아내고, CodeGemma가 수정을 제안할 것입니다.
+# app/main.py에 슬쩍 추가해 보세요
+def bad_logic(data):
+        unused_var = 123  # Ruff가 잡아낼 것임
+    return eval(data) # Bandit/Semgrep이 잡아내고, CodeGemma가 수정을 제안할 것입니다.
 
 
 
