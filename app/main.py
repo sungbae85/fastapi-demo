be1df386b0cdae31c391159c5c1763da60ba67c3
breeze <ha.sungbae@breezelab.io>
2026-01-01 13:26:04 +0900

    error include

--- app/main.py
+++ app/main.py
@@ -14,9 +14,9 @@ def hello_api(name: str):
     return hello(name)
 
 # app/main.py에 슬쩍 추가해 보세요
-#def bad_logic(data):
-#        unused_var = 123  # Ruff가 잡아낼 것임
-#    return eval(data) # Bandit/Semgrep이 잡아내고, CodeGemma가 수정을 제안할 것입니다.
+def bad_logic(data):
+        unused_var = 123  # Ruff가 잡아낼 것임
+    return eval(data) # Bandit/Semgrep이 잡아내고, CodeGemma가 수정을 제안할 것입니다.
 
 
 
